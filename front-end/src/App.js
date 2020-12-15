2c125b0576c77c60fe40041eb41c09be6a56b981
j <facebookhacker15@hotmail.com>
2020-12-15 21:08:25 +0700

    initialize node.js

---
+++ front-end/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <p>
+          Audio Description Project Main Page
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
