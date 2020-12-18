45354f1761a45b5bc038950a7eb5ab4e5ac4e450
Phatcharapon Jumruspun <darkenstardragon@Phatcharapons-MacBook-Pro.local>
2020-12-18 21:53:23 +0700

    Add seeking functionality

---
+++ front-end/src/components/SeekBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+var SeekBar = () => {
+  return (
+    <section className="SeekBar">
+      <input type="range" min="0" max="1" step="any"></input>
+    </section>
+  );
+};
+
+export default SeekBar;
---
+++ front-end/src/components/Video.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactPlayer from "react-player";
+
+var Video = () => {
+  return (
+    <section className="VideoPlayer">
+      <ReactPlayer url="test4.mp4" />
+    </section>
+  );
+};
+
+export default Video;
