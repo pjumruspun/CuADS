477f6b7649d30dcea4a3d6a2a76caaf67c6c8945
Phatcharapon Jumruspun <darkenstardragon@Phatcharapons-MacBook-Pro.local>
2020-12-18 21:53:23 +0700

    Add seeking functionality

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,68 @@
-import './App.css';
+import React, { Component } from "react";
+import ReactPlayer from "react-player";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <p>
-          Audio Description Project Main Page
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    url: null,
+    played: 0,
+  };
+
+  handleSeekMouseDown = (e) => {
+    console.log("down");
+    this.setState({ seeking: true });
+  };
+
+  handleSeekChange = (e) => {
+    console.log("Change");
+    this.setState({ played: parseFloat(e.target.value) });
+  };
+
+  handleSeekMouseUp = (e) => {
+    console.log("Up");
+    this.setState({ seeking: false });
+    this.player.seekTo(parseFloat(e.target.value));
+  };
+
+  handleProgress = (state) => {
+    console.log("onProgress", state);
+    if (!this.state.seeking) {
+      this.setState(state);
+    }
+  };
+
+  ref = (player) => {
+    this.player = player;
+  };
+
+  render() {
+    const { url, played } = this.state;
+
+    return (
+      <div className="App">
+        <header>
+          <p>Audio Description Project Main Page</p>
+        </header>
+        <ReactPlayer
+          ref={this.ref}
+          className="react-player"
+          url="test.mp4"
+          onSeek={(e) => console.log("onSeek", e)}
+        />
+        <input
+          type="range"
+          min={0}
+          max={0.99999}
+          step="any"
+          value={played}
+          onMouseDown={this.handleSeekMouseDown}
+          onChange={this.handleSeekChange}
+          onInput={this.handleSeekChange}
+          onMouseUp={this.handleSeekMouseUp}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
