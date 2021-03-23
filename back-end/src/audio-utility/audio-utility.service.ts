132d4e6fd31af0ab7247487f828bacd04efb8961
Phatcharapon Jumruspun <darkenstardragon@Phatcharapons-MacBook-Pro.local>
2021-03-23 11:56:56 +0700

    Update project document with converted audio URL

--- back-end/src/audio-utility/audio-utility.service.ts
+++ back-end/src/audio-utility/audio-utility.service.ts
@@ -16,14 +16,12 @@ ffmpeg.setFfmpegPath(pathToFfmpeg);
 
 @Injectable()
 export class AudioUtilityService {
-    async modifyAudioFromReq(@Req() request, @Res() response, @Body() modifyAudioDto: ModifyAudioDto=undefined, projectId: string = "") {
+    async modifyAudioFromReq(@Req() request, @Res() response, @Body() modifyAudioDto: ModifyAudioDto=undefined, projectId: string=undefined) {
         if (!request.files[0]) {
             const errMessage = "No file found in the request body";
             console.log(errMessage);
             return response.status(404).json(`Failed to test in service: ${errMessage}`);
         }
-
-        console.log(projectId);
 
         // File that has been uploaded in the request
         var file = request.files[0]
@@ -36,12 +34,12 @@ export class AudioUtilityService {
             if (err) console.log(`ERROR: ${err}`);
         })
 
-        await this.modifyAudio(file, tmpFilePath, file.originalname, response, modifyAudioDto);
+        await this.modifyAudio(file, tmpFilePath, file.originalname, response, modifyAudioDto, projectId);
     }
 
 
 
-    async modifyAudio(file, tmpFilePath, originalFileName, @Res() response, @Body() modifyAudioDto: ModifyAudioDto=undefined) {
+    async modifyAudio(file, tmpFilePath, originalFileName, @Res() response, @Body() modifyAudioDto: ModifyAudioDto=undefined, projectId: string=undefined) {
         // Cut the old file type from the name
         const fileName = originalFileName.slice(0, -4)
 
@@ -63,16 +61,13 @@ export class AudioUtilityService {
         }
 
         // FFMPEG operations
-        // We have to .audioFrequency(22050) for it to work, for some reason
         await ffmpeg(ffmpegInPath)
         .format('mp3') // convert to mp3
         .on("end", function () {
             // Callback when done
 
-            // Delete temp file
-            fs.unlink(tmpFilePath, function (error) { 
-                if (error) throw error;
-            })
+            // Delete temp input file
+            fs.unlink(tmpFilePath, (err) => { if (err) throw err; })
 
             // Then save to GridFS
             const stream = fs.createReadStream(ffmpegOutPath);
@@ -82,12 +77,23 @@ export class AudioUtilityService {
 
             axios.post(`http://localhost:3001/files`, fd, { headers: headers })
             .then((res) => {
-                console.log(res.data);
-                const fileid = res.data[0].id;
-                return axios.post(`http://localhost:3001/files/${fileid}`);
+                // Delete temp output file
+                fs.unlink(ffmpegOutPath, (err) => { if (err) throw err; });
+
+                if(!projectId) // Case convert without projectId
+                {
+                    // Returns saved object
+                    return response.status(201).json(res.data)
+                }
+
+                // If projectId is not undefined, try to update projects document
+                const audioURL = `http://localhost:3001/files/${res.data[0].id}`;
+                const putURL = `http://localhost:3001/projects/${projectId}`;
+                return axios.put(putURL, { 'originalAudioURL': audioURL });
             })
             .then((res) => {
-
+                // Return update results
+                return response.status(201).json(res.data)
             });
         })
         .on("error", function (err) {
@@ -99,7 +105,5 @@ export class AudioUtilityService {
         })
         .audioFilters(volumeFilter) // could chain more modification later
         .saveToFile(ffmpegOutPath)
-
-        // console.log(`file: ${file}`);
     }
 }
