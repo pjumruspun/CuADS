a8820624982c7d666febcacb76d0e2b19c8ffb41
Phatcharapon Jumruspun <darkenstardragon@Phatcharapons-MacBook-Pro.local>
2021-05-06 14:27:13 +0700

    Fix seeking by storing in /upload instead of GridFS

--- back-end/src/files/files.module.ts
+++ back-end/src/files/files.module.ts
@@ -1,16 +1,30 @@
 import { Module } from '@nestjs/common';
-import { FilesService } from './files.service';
 import { FilesController } from './files.controller';
 import { MulterModule } from '@nestjs/platform-express';
 import { GridFsMulterConfigService } from './multer-config.service';
+import { diskStorage } from 'multer';
 
+var path = require('path')
 @Module({
     imports: [
         MulterModule.registerAsync({
-            useClass: GridFsMulterConfigService,
+            useFactory: () => ({
+                storage: diskStorage({
+                    destination: './upload',
+                    filename: editFileName,
+                }),
+            }),
         })
     ],
-    providers: [FilesService, GridFsMulterConfigService],
+    providers: [GridFsMulterConfigService],
     controllers: [FilesController]
 })
 export class FilesModule {}
+
+export const editFileName = (req, file, callback) => {
+    const fileExtName = path.extname(file.originalname);
+    const randomName = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    const newName = `${randomName}${fileExtName}`
+    console.log(newName);
+    callback(null, newName);
+};
