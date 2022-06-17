cb4f9ec731591dfb91a1e76b5e186bb4aa352cc8
Phatcharapon Jumruspun <darkenstardragon@Phatcharapons-MacBook-Pro.local>
2022-06-17 18:51:08 +0700

    Move old components to /old, starting MenuBar

---
+++ front-end/src/components/menubar/MenuBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MenuBar() {
+  return (
+    <nav className="menubar">
+      <ul className="menubar-menu"></ul>
+    </nav>
+  );
+}
+
+export default MenuBar;
