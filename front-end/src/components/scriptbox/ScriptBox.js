3026399674703371f99f54c2316d0a959e15622f
Phatcharapon Jumruspun <darkenstardragon@Phatcharapons-MacBook-Pro.local>
2022-06-18 12:28:08 +0700

    Add time boxes

--- front-end/src/components/scriptbox/ScriptBox.js
+++ front-end/src/components/scriptbox/ScriptBox.js
@@ -1,15 +1,40 @@
 import { Grid } from "@material-ui/core";
 import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Paper from "@material-ui/core/Paper";
 
-var scriptBoxStyle = {
-  padding: "12px",
-};
-
-var rootStyle = {
+// Div style
+const rootStyle = {
   backgroundColor: "#333333",
   display: "flex",
   height: "100%",
   width: "50%",
+};
+
+// Main grid style
+const scriptBoxStyle = {
+  padding: "12px",
+};
+
+// Preview text style
+const inputLabelProps = {
+  style: {
+    color: "gray",
+  },
+};
+
+// Typed text style
+const inputProps = {
+  style: {
+    color: "white",
+  },
+};
+
+const timeInputLabelProps = {
+  style: {
+    color: "gray",
+    shrink: true,
+  },
 };
 
 function ScriptBox() {
@@ -22,9 +47,48 @@ function ScriptBox() {
         alignItems="flex-start"
         style={scriptBoxStyle}
       >
-        <h4>Script</h4>
-        <h4>Time</h4>
-        <h4>From To</h4>
+        <TextField
+          id="input-text"
+          label="Input Text"
+          variant="filled"
+          InputLabelProps={inputLabelProps}
+          InputProps={inputProps}
+          color="white"
+          fullWidth
+          multiline
+          minRows={2}
+          maxRows={2}
+        ></TextField>
+        <Grid container justify="flex-start" alignItems="center">
+          <Grid item xs={4} md={1}>
+            Time
+          </Grid>
+          <Grid item xs={4} md={4}>
+            <TextField
+              id="start-time"
+              label="Start"
+              variant="filled"
+              InputLabelProps={timeInputLabelProps}
+              InputProps={inputProps}
+              color="white"
+              size="small"
+              style={{ width: "90%" }}
+            ></TextField>
+          </Grid>
+          <Grid item xs={4} md={4}>
+            <TextField
+              id="end-time"
+              label="End"
+              variant="filled"
+              InputLabelProps={timeInputLabelProps}
+              InputProps={inputProps}
+              color="white"
+              size="small"
+              style={{ width: "90%" }}
+            ></TextField>
+          </Grid>
+        </Grid>
+
         <h4>Adjustment</h4>
         <h4>Volume</h4>
         <h4>Speed</h4>
